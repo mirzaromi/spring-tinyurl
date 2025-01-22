9056321580b99d743de90b608e7a2e3f205a1a16
mirz <mirza.romi@doku.com>
2025-01-22 14:55:48 +0700

    add log for total process time

--- src/main/java/org/mirza/tinyurl/controller/UrlController.java
+++ src/main/java/org/mirza/tinyurl/controller/UrlController.java
@@ -2,6 +2,7 @@ package org.mirza.tinyurl.controller;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mirza.tinyurl.dto.BaseResponse;
 import org.mirza.tinyurl.service.UrlService;
 import org.springframework.http.HttpStatus;
@@ -12,14 +13,19 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 @RequestMapping("/url-shortener")
 @RequiredArgsConstructor
+@Slf4j
 public class UrlController {
 
     private final UrlService urlService;
 
     @PostMapping("shorten")
     public ResponseEntity<BaseResponse<String>> shortenUrl(@NotBlank String longUrl) {
+        long start = System.currentTimeMillis();
 
         String shortUrl = urlService.generateEncodedUrl(longUrl);
+
+        long executionTime = System.currentTimeMillis() - start;
+        log.info("task executed in {} ms", executionTime);
 
         BaseResponse<String> response = new BaseResponse<>();
         response.setCode(HttpStatus.OK.value());
@@ -31,7 +37,12 @@ public class UrlController {
 
     @GetMapping("/get-long-url")
     public ResponseEntity<BaseResponse<String>> getLongUrl(String shortUrl) {
+        long start = System.currentTimeMillis();
+
         String longUrl = urlService.getLongUrl(shortUrl);
+
+        long executionTime = System.currentTimeMillis() - start;
+        log.info("task executed in {} ms", executionTime);
 
         BaseResponse<String> response = new BaseResponse<>();
         response.setCode(HttpStatus.OK.value());
